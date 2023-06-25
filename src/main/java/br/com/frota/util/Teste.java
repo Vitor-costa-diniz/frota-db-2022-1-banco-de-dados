4f10289fb69831702a4fbc184be49d5edd56287b
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2023-06-24 22:59:07 -0300

    Method to create tables at DataBase

--- src/main/java/br/com/frota/util/Teste.java
+++ src/main/java/br/com/frota/util/Teste.java
@@ -1,4 +1,5 @@
 package br.com.frota.util;
+
 import br.com.frota.DAO.*;
 
 public class Teste {
@@ -7,22 +8,13 @@ public class Teste {
     static RotaDAO rota = new RotaDAO();
     static TarefaRotaDAO tarefaRota = new TarefaRotaDAO();
     static TimeRotaDAO timeRotaDAO = new TimeRotaDAO();
-
     static FuncionarioDAO funcionario = new FuncionarioDAO();
+
     public static void main(String[] args) throws Exception {
+//        ConexaoDB.criarTabelas(); // Chamada para criar as tabelas
 
-//        String str="2018-09-01 09:01:15";
-//        Timestamp timestamp= Timestamp.valueOf(str);
-//        System.out.println("2. value of Timestamp : "+timestamp);
-//
-//        TarefaRota rota1 = new TarefaRota(2, "batata", timestamp, timestamp, 1 );
-//        tarefaRota.insertTarefaRota(rota1);
+        System.out.println(timeRotaDAO.selectTimeRotaById(0));
 
-//        TimeRota timeRota = new TimeRota(1, 1, 1 );
-//        timeRotaDAO.setInsertTimeRotaSQL(timeRota);
-
-        System.out.println(timeRotaDAO.selectTimeRotaById(1));
-
-
+        // Restante do código...
     }
 }
